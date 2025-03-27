f6def5ce6f878831e76e80e034001908221d9f97
Sanjhanna Bethi <sanjhannabethi@gmail.com>
2025-03-27 21:46:48 +0530

    Implement basic utility methods to add, view and search books

--- DigitalLibrarySystem/src/service/LibraryService.java
+++ DigitalLibrarySystem/src/service/LibraryService.java
@@ -1,5 +1,46 @@
 package service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import model.Book;
+
 public class LibraryService {
-    
+    private List<Book> books;
+
+    public LibraryService() {
+        books = new ArrayList<>();
+    }
+
+    // Add a book if the Book ID is unique
+    public boolean addBook(Book book) {
+        if(findBookById(book.getBookId()).isPresent()){
+            System.out.println("Error: Book ID already exists!");
+            return false;
+        }
+        books.add(book);
+        return true;
+    }
+
+    // View all books
+    public List<Book> getAllBooks() {
+        return books;
+    }
+
+    // Search by ID
+    public Optional<Book> findBookById(String bookId) {
+        return books.stream().filter(b -> b.getBookId().equalsIgnoreCase(bookId)).findFirst();
+    }
+
+    // Search by title (partial match allowed)
+    public List<Book> findBooksByTitle(String title) {
+        List<Book> result = new ArrayList<>();
+        for(Book book : books) {
+            if(book.getTitle().toLowerCase().contains(title.toLowerCase())) {
+                result.add(book);
+            }
+        }
+        return result;
+    }
 }
